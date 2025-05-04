82908795bb73f06a8f6f5abce9a0cd0389d0ce26
Haoli Yin <haoli.yin@vanderbilt.edu>
2025-05-04 22:28:36 +0000

    modify sequence packing script

--- mm_sequence_packing/multiprocess_sequence_packing_image_to_pil.sh
+++ mm_sequence_packing/multiprocess_sequence_packing_image_to_pil.sh
@@ -2,9 +2,8 @@
 start_id=$1
 num_proc=$2 # on our server we use 4; you can enlarge this based on you cpu cores
 tars_per_gpu=$3 # the number of tar files processed by each process
-
-
-gpu_name=$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader)
+dataset=$4
+data_root=$5
 
 start=`date +"%s"`
 
@@ -12,20 +11,24 @@ i=0
 while [ $i -lt $num_proc ]; do
   {
     echo $i
-    if [ "$4" == "obelics" ]; then
-      mkdir Open-Qwen2VL-Data/obelics/obelics_single_pkl_pil
-      python mm_sequence_packing/sequence_packing_image_to_pil.py --tar-file-path Open-Qwen2VL-Data/obelics_webdataset --save-path Open-Qwen2VL-Data/obelics_single_pkl_pil --gpu-id $(($i + $start_id)) --tars-per-gpu ${tars_per_gpu}
-    elif [ "$4" == "synthdog" ]; then
-      mkdir Open-Qwen2VL-Data/synthdog-en/synthdog_single_pkl_pil 
-      python mm_sequence_packing/sequence_packing_image_to_pil.py --tar-file-path Open-Qwen2VL-Data/synthdog_webdataset --save-path Open-Qwen2VL-Data/synthdog_single_pkl_pil --gpu-id $(($i + $start_id)) --tars-per-gpu ${tars_per_gpu}
-    elif [ "$4" == "ccs" ]; then
-      python mm_sequence_packing/sequence_packing_image_to_pil.py --tar-file-path Open-Qwen2VL-Data/ccs_webdataset --save-path Open-Qwen2VL-Data/ccs_single_pkl_pil --gpu-id $(($i + $start_id)) --tars-per-gpu ${tars_per_gpu}
-    elif [ "$4" == "laion" ]; then
-      python mm_sequence_packing/sequence_packing_image_to_pil.py --tar-file-path Open-Qwen2VL-Data/laion_webdataset --save-path Open-Qwen2VL-Data/laion_single_pkl_pil --gpu-id $(($i + $start_id)) --tars-per-gpu ${tars_per_gpu}
-    elif [ "$4" == "datacomp-dfn" ]; then
-      python mm_sequence_packing/sequence_packing_image_to_pil.py --tar-file-path Open-Qwen2VL-Data/datacomp_medium_dfn_webdataset --save-path Open-Qwen2VL-Data/datacomp_dfn_single_pkl_pil --gpu-id $(($i + $start_id)) --tars-per-gpu ${tars_per_gpu}
+    if [ "$dataset" == "obelics" ]; then
+      mkdir ${data_root}/obelics/obelics_single_pkl_pil
+      python mm_sequence_packing/sequence_packing_image_to_pil.py --tar-file-path ${data_root}/obelics_webdataset --save-path ${data_root}/obelics_single_pkl_pil --gpu-id $(($i + $start_id)) --tars-per-gpu ${tars_per_gpu}
+    elif [ "$dataset" == "synthdog" ]; then
+      mkdir ${data_root}/synthdog-en/synthdog_single_pkl_pil 
+      python mm_sequence_packing/sequence_packing_image_to_pil.py --tar-file-path ${data_root}/synthdog_webdataset --save-path ${data_root}/synthdog_single_pkl_pil --gpu-id $(($i + $start_id)) --tars-per-gpu ${tars_per_gpu}
+    elif [ "$dataset" == "ccs" ]; then
+      mkdir ${data_root}/ccs/ccb_single_pkl_pil
+      python mm_sequence_packing/sequence_packing_image_to_pil.py --tar-file-path ${data_root}/ccs_webdataset --save-path ${data_root}/ccs_single_pkl_pil --gpu-id $(($i + $start_id)) --tars-per-gpu ${tars_per_gpu}
+    elif [ "$dataset" == "laion" ]; then
+      mkdir ${data_root}/laion/laion_single_pkl_pil
+      python mm_sequence_packing/sequence_packing_image_to_pil.py --tar-file-path ${data_root}/laion_webdataset --save-path ${data_root}/laion_single_pkl_pil --gpu-id $(($i + $start_id)) --tars-per-gpu ${tars_per_gpu}
+    elif [ "$dataset" == "datacomp-dfn" ]; then
+      mkdir ${data_root}/datacomp-medium/dfn_single_pkl_pil
+      python mm_sequence_packing/sequence_packing_image_to_pil.py --tar-file-path ${data_root}/datacomp_medium_dfn_webdataset --save-path ${data_root}/datacomp_dfn_single_pkl_pil --gpu-id $(($i + $start_id)) --tars-per-gpu ${tars_per_gpu}
     else
-      python mm_sequence_packing/sequence_packing_image_to_pil.py --tar-file-path Open-Qwen2VL-Data/datacomp_medium_mlm_filter_su_85_union_dfn_webdataset/ --save-path Open-Qwen2VL-Data/datacomp_hq_single_pkl_pil --gpu-id $(($i + $start_id)) --tars-per-gpu ${tars_per_gpu}
+      mkdir ${data_root}/datacomp-medium/hq_single_pkl_pil
+      python mm_sequence_packing/sequence_packing_image_to_pil.py --tar-file-path ${data_root}/datacomp_medium_mlm_filter_su_85_union_dfn_webdataset/ --save-path ${data_root}/datacomp_hq_single_pkl_pil --gpu-id $(($i + $start_id)) --tars-per-gpu ${tars_per_gpu}
     fi
   } &
   i=$(($i + 1))
